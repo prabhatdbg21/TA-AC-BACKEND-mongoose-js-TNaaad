0a5f603690fbdbf0b82ccc430ff6547ed3e42cef
Prabhat <prabhatdbg21@gmail.com>
2023-04-01 21:22:07 +0530

    All Good

---
+++ block-BNaacj/sample/models/user.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    password: {type: String, minlength: 5, maxlength:15},
+    createdAt: {type: Date, default: Date.now}
+}, 
+{timestamps: true} 
+)  
